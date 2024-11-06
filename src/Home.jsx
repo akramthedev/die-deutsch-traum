d30e9ca0a00b9ccc5bef7c95703a5477baeab69f
akramthedev <162736569+akramthedev@users.noreply.github.com>
2024-11-06 18:19:16 +0100

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -80,7 +80,7 @@ const Home = () => {
     <div className='Home'>
       <div className="Home2">
         <h1>
-          {isTimeUp ? "Timer completed" : "Deutsch-Prüfungsergebnisse"}    
+          {isTimeUp ? "Timer completed" : "Deutsch-Prüfungsergebnisse : service@telc.net"}    
         </h1>
         <div className="Timer">
           {formatTime()}
