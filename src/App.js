927869f1c28c911d2b8cf0d81ff5df2eaf441e85
akramthedev <162736569+akramthedev@users.noreply.github.com>
2024-11-09 21:09:25 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './Home.jsx';
 import Dokument from './Dokument.jsx';
-
+import Todo from "./Todo.jsx";
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/Dokument' element={<Dokument />} />
+          <Route path='/todo' element={<Todo />} />
         </Routes>
       </BrowserRouter>
     </div>
