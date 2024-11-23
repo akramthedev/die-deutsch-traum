59f8ede65088d8afe2a48dc1a4be4ab0da2a0e1f
akramthedev <162736569+akramthedev@users.noreply.github.com>
2024-11-23 01:16:02 +0100

    Update Dokument.jsx

--- src/Dokument.jsx
+++ src/Dokument.jsx
@@ -10,7 +10,8 @@ import LN from './LN.pdf'
 import LC from './LC.pdf'
 import DC from './DC.pdf'
 import BAC from './BAC.pdf'
-
+import Zer from './zertifikat.pdf'
+ 
 
 import './App.css';
 
@@ -18,44 +19,60 @@ const Dokument = () => {
   return (
     <div className='Dokument'>
       <h1>Wesentliches Dokument</h1>
-      <br /><br />
+      <br /> 
       <a>
         Numéro enregistrement Botschaft : 22663930 
-      </a> 
-       <a>
+      </a>
+             <br /> 
+<a>
         Emailing the TELC : service@telc.net 
       </a> 
-       <a>
+             <br /> 
+<a>
         Numéro Passeport : NS1562494  
       </a>
-      <a href={LettreRecommendation} download="Lettre Recommendation Rachid.pdf">
+            <br /> 
+<a href={Zer} download="ZertifikatB1.pdf">
+        Zertifikat Deutsch B1 
+      </a>
+           <br /> 
+ <a href={LettreRecommendation} download="Lettre Recommendation Rachid.pdf">
         Lettre Recommendation - Rachid Dakir
       </a>
-      <a href={LettreRecommendation2} download="Lettre Recommendation Imane.pdf">
+           <br /> 
+ <a href={LettreRecommendation2} download="Lettre Recommendation Imane.pdf">
         Lettre Recommendation - Imane Lmati
       </a>
-      <a href={FicheLST} download="ficheLST.pdf">
+          <br /> 
+  <a href={FicheLST} download="ficheLST.pdf">
         Fiche Module LST
       </a>
-      <a href={FicheDEUST} download="FicheDEUST.pdf">
+         <br /> 
+   <a href={FicheDEUST} download="FicheDEUST.pdf">
         Fiche Module DEUST
       </a>
-      <a href={CV} download="cv.pdf">
+           <br /> 
+ <a href={CV} download="cv.pdf">
         C.V
       </a>
-      <a href={DC} download="desut certificate.pdf">
+           <br /> 
+ <a href={DC} download="desut certificate.pdf">
         DEUST Certificate
       </a>
-      <a href={LC} download="lst certificate.pdf">
+        <br /> 
+    <a href={LC} download="lst certificate.pdf">
         LST Certificate
       </a>
-      <a href={DN} download="deust notes.pdf">
+          <br /> 
+  <a href={DN} download="deust notes.pdf">
         DEUST Notes
       </a>
-      <a href={LN} download="lst notes.pdf">
+          <br /> 
+  <a href={LN} download="lst notes.pdf">
         LST Notes
       </a>
-      <a href={BAC} download="bac.pdf">
+         <br /> 
+   <a href={BAC} download="bac.pdf">
         BAC
       </a>
 
