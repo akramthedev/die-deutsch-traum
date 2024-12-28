3da5942ad1379a3ce2326802b3bb5cc70402e05e
akramthedev <162736569+akramthedev@users.noreply.github.com>
2024-12-28 13:18:12 +0100

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import {useNavigate} from 'react-router-dom'
 const Home = () => {
-  const targetDate = new Date('2025-03-31T00:00:00');  
+  const targetDate = new Date('2025-02-22T00:00:00');  
   const nav = useNavigate();
   const calculateTimeLeft = () => {
     const now = new Date();
@@ -69,7 +69,7 @@ const Home = () => {
   return (
     <div className='Home'>
       <div className="Home2">
-        <h1>telc B2</h1>
+        <h1>toefl Band 90</h1>
         <div className="Timer">
           {formatTime()}
         </div>
