a3faa852eb09fecd2a8d2c9ceefe19611a915700
akramthedev <162736569+akramthedev@users.noreply.github.com>
2025-08-15 15:27:17 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Dokument from './Dokument.jsx';
 import Todo from "./Todo.jsx";
 import TimeManag from "./TimeManag.jsx";
 import TimeManag2 from "./TimeManag2.jsx";
+import DokumentTelc from "./DokumentTelc.jsx"
+
 
 function App() {
   return (
@@ -13,9 +15,10 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/Dokument' element={<Dokument />} />
+          <Route path='/Dokument-Telc' element={<DokumentTelc />} />
           <Route path='/todo' element={<Todo />} />
-            <Route path="/TimeManag" element={<TimeManag />} />
-            <Route path="/TimeManag2" element={<TimeManag2 />} />
+          <Route path="/TimeManag" element={<TimeManag />} />
+          <Route path="/TimeManag2" element={<TimeManag2 />} />
         </Routes>
       </BrowserRouter>
     </div>
