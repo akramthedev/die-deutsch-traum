79a3b629c330fb6f5aec0ccc9df0f49e97b6243b
akramthedev <162736569+akramthedev@users.noreply.github.com>
2025-08-15 15:35:13 +0100

    Create DokumentTelc.jsx

---
+++ src/DokumentTelc.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+const Dokument = () => {
+  return (
+    <div className='Dokument'>
+      
+      <h1>Telc Dokument</h1>
+      
+      <a href="https://drive.google.com/drive/u/0/folders/11eCkDvhYyNr7aJSTcUljW2eeMWHz0ezh?fbclid=IwY2xjawG64OwBHRjCMumLmKZ1_-wdy-4z-hhuu0tPfqJqnY6nhuhNP7UWIggqoTGt934UzQ_aem_Af-J7unl6NqFXmFirFWbLL5hecqyVu0L1EDfgM2_Hg3cQbud8moFXu0JO4eZJJ6B_9D5e7e_OqG8jcv8kfktV391&sfnsn=wa">
+        Essential Drive
+      </a>
+
+      <a href="https://drive.google.com/drive/folders/1NgRuAnROT4ED9yGJJP9fvWbeltLGCCmR?usp=sharing">
+        Andere Drive
+      </a>
+
+      <a href="https://drive.google.com/drive/folders/1G8vR825DwG-FmSon2JEpZibD6_1SZAut">
+        Horen Drive
+      </a>
+      
+    </div>
+  );
+};
+
+export default Dokument;
